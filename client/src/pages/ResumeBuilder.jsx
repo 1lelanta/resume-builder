1a3c675068734d30d7cc6eec2cb5f43ca004f744
1lelanta <kekikoki4@gmail.com>
2026-02-18 21:35:00 +0300

    mount template selector inside resume builder

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -14,6 +14,7 @@ import {
 } from 'lucide-react'
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
+import TemplateSelector from '../components/TemplateSelector'
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams()
@@ -88,8 +89,10 @@ const ResumeBuilder = () => {
 
             {/* section navigation */}
             <div className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'>
-              <div>
-                
+              <div className='flex justify-between items-center mb-6 border-b
+              border-gray-300 py-1'>
+                <TemplateSelector selectedTemplate={resumeData.template} onChange=
+                {(template)=>setResumeData(prev=>({...prev,template}))}/>
               </div>
 
               <div className='flex items-center'>
