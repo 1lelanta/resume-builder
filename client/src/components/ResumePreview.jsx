de7fae24d15d2bbee3ed2fa3724928e44993d123
1lelanta <kekikoki4@gmail.com>
2026-02-18 19:54:31 +0300

    add a function that render each template

--- client/src/components/ResumePreview.jsx
+++ client/src/components/ResumePreview.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import ClassicTemplate from "./templates/ClassicTemplate";
+import ModernTemplate from "./templates/ModernTemplate"
+import MinimalTemplate from "./templates/MinimalTemplate"
+import MinimalImageTemplate from "./templates/MinimalImageTemplate"
 
 const ResumePreview = ({data, template, accentColor, classes = ""}) => {
+
+    const renderTemplate = ()=>{
+        switch(template){
+            case "modern":
+                return <ModernTemplate data={data} accentColor={accentColor}/>
+            case "minimal":
+                return <MinimalTemplate data={data} accentColor={accentColor}/>
+            case "minimal-image":
+                return <MinimalImageTemplate data={data} accentColor={accentColor}/>;
+            default:
+                return
+        }
+    }
   return (
     <div className='w-full bg-gray-100'>
         <div id='resume-preview' className={"border border-gray-200 print:shadow-none print:border-none" + classes}>
 
         </div>
-
     </div>
   )
 }
