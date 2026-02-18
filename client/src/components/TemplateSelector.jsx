3b3313ae51ea5600d910ef421afc53ba5c99f0dd
1lelanta <kekikoki4@gmail.com>
2026-02-18 21:08:06 +0300

    add template for minimal image

--- client/src/components/TemplateSelector.jsx
+++ client/src/components/TemplateSelector.jsx
@@ -13,6 +13,16 @@ const TemplateSelector = ({ selectedTemplate, onChange}) => {
             id: "modern",
             name: "Modern",
             preview: "Modern Resume format"
+        },
+        {
+            id:"minimal-image",
+            name:"Minimal Image",
+            preview: "Minimal design with a single image and clean typography"
+        },
+        {
+            id: "minimal",
+            name: "Minimal",
+            preview: "Ultra-clean design that puts your content front and center"
         }
     ]
   return (
