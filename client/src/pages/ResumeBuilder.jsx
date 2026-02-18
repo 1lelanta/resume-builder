080fd68a640ebb89c8dd7dda48d692b20a4d4f42
1lelanta <kekikoki4@gmail.com>
2026-02-18 22:12:32 +0300

    mount color picker  and pass props inside resume builder

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -15,6 +15,7 @@ import {
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
 import TemplateSelector from '../components/TemplateSelector'
+import ColorPicker from '../components/ColorPicker'
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams()
@@ -92,6 +93,9 @@ const ResumeBuilder = () => {
               <div className='flex items-center gap-2'>
                 <TemplateSelector selectedTemplate={resumeData.template} onChange=
                 {(template)=>setResumeData(prev=>({...prev,template}))}/>
+
+                <ColorPicker selectedColor={resumeData.accent_color} 
+                onChange={(color)=>setResumeData(prev =>({...prev,accent_color:color}))}/>
               </div>
 
               <div className='flex items-center'>
