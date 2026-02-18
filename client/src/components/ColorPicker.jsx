f476fc146fb529d19f719f4bdbe85ed7eccda1a2
1lelanta <kekikoki4@gmail.com>
2026-02-18 21:52:32 +0300

    add different color

---
+++ client/src/components/ColorPicker.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ColorPicker = () => {
+    const colors = [
+        {name: "Blue", value: "#3B82F6" },
+        {name: "Indigo", value: "#6366F1" },
+        {name: "Purple", value: "#885CF6" },
+        {name: "Green", value: "#10B981" },
+        {name: "Red", value: "#EF4444" },
+        {name: "Blue", value: "#3B82F6" },
+        {name: "Orange", value: "#F97316" },
+        {name: "Teal", value: "#14B8A6"},
+        {name: "Pink", value: "#EC4899"},
+        {name: "Gray", value: "#6B7280"},
+        {name: "Black", value: "#1F2937"}
+    ]
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ColorPicker
